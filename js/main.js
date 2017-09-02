ea0b7601c99b68ba77007033124c40bf0027835c
Lulu Wang <lulu.wang25@gmail.com>
2017-09-01 22:47:35 -0700

    try js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showDiv(){
+  var disply = document.getElementById('abtMe').style.display;
+  if (disply == "block") {
+    document.getElementById('abtMe').style.display = "none";
+  } else {
+    document.getElementById('abtMe').style.display = "block";
+  }
+}
