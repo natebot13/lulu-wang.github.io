a1f5f42170de0c53ef37320298e0cec4e2d71e7a
Nathan Pannell <nathan.pannell@berkeley.edu>
2018-01-12 16:15:00 -0800

    added portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,58 @@
+var images = ['stardust.jpg',
+              'print3.jpg',
+              'mikasa.png',
+              'brs.jpg',
+              'sky.jpg',
+              'azure.jpg',
+              'mirai.jpg',
+              'rakkun.jpg',
+              'dvapost2.jpg',
+              'fashion1.jpg',
+              'fashion2.jpg',
+              'fashion3.jpg',
+              'demon.jpg',
+              'friskwip.jpg',
+              '1.jpg',
+              '2.jpg',
+              '3.jpg',
+              '6.jpg',
+              '5.jpg',
+              '4.jpg',
+              'ori.jpg',
+              'receptacle.jpg',
+              'sketch1.jpg',
+              'shiro.jpg'
+              ].map(function(i) {
+                return $('<div>').addClass('grid-item')
+                  .append($('<img>')
+                    .attr('src', 'images/portfolioimages/' + i))
+              });
+
+var uiux = []
+
+function clearGrid() {
+  $('.grid').html($('<div>').addClass('grid-sizer'));
+}
+
+function fillGrid(things) {
+  $('.grid').append(things);
+}
+
+$(document).ready(function() {
+  fillGrid(images);
+  $('#uiux').click(function(e) {
+    clearGrid();
+  });
+  // init Masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer',
+    transitionDuration: 1000,
+    stagger: 50
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });
+});
